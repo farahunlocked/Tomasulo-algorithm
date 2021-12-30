9aeac1a7fad71dc892810caeb9a020cdb699894e
farahunlocked <farahhassan12@hotmail.com>
2021-12-31 01:13:47 +0200

    update

--- src/InstructionSet.java
+++ src/InstructionSet.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 public class InstructionSet {
     private static ArrayList<String> instructions;
 
+
     public InstructionSet(ArrayList<String> input){
         instructions=input;
     }
--- src/ReservationStation.java
+++ src/ReservationStation.java
@@ -1,49 +1,11 @@
 public class ReservationStation {
-    public String type; //add nd sub or mult nd divide (general type)
-    public boolean busy;
-    public String operation;
-    public long Vj;
-    public long Vk;
-    public String Qj;
-    public String Qk;
-    public long address;
-    public long result;
+    private static Station[] mult_div;
+    private static Station[] add_sub;
 
 
 
-    public ReservationStation(String type){
-        this.type = type;
-        busy = false;
-        operation = null;
-        Vj =0;
-        Vk =0;
-        address = 0;
-        Qj = null;
-        Qk = null;
+    public ReservationStation(int size){
 
     }
-    // once done delete instruct from reserv bus
-    public void removeInstruction(){
-        busy = false;
-        operation = null;
-        Vj = 0;
-        Vk =0;
-        address = 0;
-        Qj =null;
-        Qk = null;
-
-    }
-
-
-    // checks if the op for the instructi0n are alllll ready and hasnt exec yet
-    public boolean ready(){
-        return (busy && Qj == null && Qk == null );
-    }
-
-    // method for printings
-    public void printContent(){
-
-    }
-
 
 }
